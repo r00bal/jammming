2e722632792c626e04df3fc8f357d0cbf3d8a1e4
r00bal <frontendpit@gmail.com>
2017-10-21 13:43:36 +0200

    Pass the playlist tracks from the Playlist component to the TrackList component.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -9,7 +9,7 @@ class Playlist extends Component {
     return (
       <div className={'New Playlist'}>
         <input defaultValue="New Playlist"/>
-        <TrackList />
+        <TrackList tracks={this.props.playlistTracks}/>
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
       </div>
     );
