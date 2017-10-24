f2af19821a234378994b4051738e2791951390a7
r00bal <frontendpit@gmail.com>
2017-10-24 13:01:39 +0200

    Add a method called handleNameChange
    -The method accept an event that is triggered by an onChange attribute
    in the Playlist component's <input> element.
    -Inside the method, call .onNameChange() with the event target's value
    (from the <input> element).

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,10 +5,17 @@ import TrackList from '../TrackList/TrackList';
 
 
 class Playlist extends Component {
+
+  handleNameChange(event) {
+    const playlistName = event.target.value;
+    console.log(playlistName);
+    this.props.onNameChange(event.target.value)
+  }
+
   render() {
     return (
       <div className={'New Playlist'}>
-        <input defaultValue="New Playlist"/>
+        <input defaultValue="New Playlist" onChange={this.handleNameChange}/>
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove}/>
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
       </div>
