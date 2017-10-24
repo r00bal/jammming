56b1215e5ac0d7079f065d34c18d3e319a124c16
r00bal <frontendpit@gmail.com>
2017-10-24 17:20:09 +0200

    Bind the current value of this to .savePlaylist()

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -35,6 +35,7 @@ class App extends Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
