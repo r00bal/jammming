656d29125f466d529034a8b7d70bc443c470bdd1
r00bal <frontendpit@gmail.com>
2017-10-25 14:00:12 +0200

    Create a method called getAccessToke
    -Check if the user's access token is already set.
    If it is, return the value saved to access token.
    -Above the empty object, declare an empty variable
    that will hold the user's access token.
    
    n

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,8 +1,12 @@
 
-
+const token = 'USER_TOKEN';
 
  const Spotify = {
-
+   getAccessToken() {
+     if (accessToken) {
+      return new Promise(resolve => resolve(accessToken));
+    }
+   }
  }
 
  export default Spotify;
